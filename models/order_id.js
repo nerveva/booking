4d1e3bebfb706d3258eaefc4b37ef370d1155f80
nerveva <nervx@sina.com>
2014-04-26 20:28:43 +0800

    cancel finsih

---
+++ models/order_id.js
@@ -0,0 +1,25 @@
+var crypto = require('crypto');
+var mongoose = require('mongoose');
+
+var vSchema = {
+    OrderId : Number,
+    Tag : String
+};
+
+var orderIdSchema = new mongoose.Schema(vSchema, {
+    collection: 'orderIds'
+});
+
+var orderIdModel = mongoose.model('OrderId', orderIdSchema);
+
+exports.getId = function(callback){
+    // update
+    orderIdModel.findOneAndUpdate({Tag : "orderId"}, {$inc : {OrderId : 1}}, {upsert : true}, function (err, oId) {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, oId.OrderId);
+        console.log(oId.OrderId);
+    });
+};
+
